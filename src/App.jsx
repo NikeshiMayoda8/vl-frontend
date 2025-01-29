6e3c37386607dce9fd2dca27c13a1f70c8246fab
NikeshiMayoda8 <91651837+NikeshiMayoda8@users.noreply.github.com>
2025-01-30 00:40:16 +0530

    add new route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Home'
 import {   BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NumberCard from './NumberCard'
 import Story from './Story'
+import Login from './Login'
 
 function App() {
  
@@ -11,9 +12,10 @@ function App() {
   return (
     <Router>
     <Routes>
-      <Route path="/home" element={<Home/>}/>
+      <Route path="/" element={<Home/>}/>
       <Route path="/number-card" element={<NumberCard/>}/>
       <Route path="/story" element={<Story/>}/>
+      <Route path="/login" element={<Login/>}/>
       </Routes>
     </Router>
   )
