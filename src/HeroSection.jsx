e0761cdb5f5fbb6933ba7977b06c9a326212127b
NikeshiMayoda8 <91651837+NikeshiMayoda8@users.noreply.github.com>
2025-02-01 12:27:40 +0530

    add the cursor pointer to the button

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -19,7 +19,7 @@ export default function HeroSection() {
     <div className="relative z-10 flex  items-center justify-center h-full">
       <button
         onClick={handleClick}
-        class="shadow-[6px_6px_12px_#df6869] p-3 rounded-md  active:shadow-[inset_4px_4px_12px_#df6869] text-[#f15771] font-bold w-[100px] "
+        class="cursor-pointer shadow-[6px_6px_12px_#df6869] p-3 rounded-md  active:shadow-[inset_4px_4px_12px_#df6869] text-[#f15771] font-bold w-[100px] "
       >
         <img
           src="src/assets/images/gift.png"
